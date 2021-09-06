ce57041168b8cbd68206ae3bdf3c92dab147c3c9
Leo Luna Baima <leobaima01@gmail.com>
2021-09-06 20:05:24 -0300

    Web socket adicionado

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io'
+import path from 'path';
 
 import './database';
 import { routes } from './routes';
@@ -6,8 +9,24 @@ import { routes } from './routes';
 const app = express();
 const port = 3333;
 
+app.use(express.static(path.join(__dirname, "..", "public")))
+app.set("views", path.join(__dirname, "..", "public"))
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get('/pages/client', (req, res) => {
+    return res.render("html/client.html")
+})
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+    console.log("Se conectou", socket.id);
+})
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+http.listen(port, () => console.log(`Server running on port ${port}`));
