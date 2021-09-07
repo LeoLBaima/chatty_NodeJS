a3d29e80945ae869d8867376f9a54696589c1302
Leo Luna Baima <leobaima01@gmail.com>
2021-09-06 21:12:38 -0300

    ajustes

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,4 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io'
-import path from 'path';
+import { http } from './http'
+import './websocket/client'
 
-import './database';
-import { routes } from './routes';
-
-const app = express();
-const port = 3333;
-
-app.use(express.static(path.join(__dirname, "..", "public")))
-app.set("views", path.join(__dirname, "..", "public"))
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-
-app.get('/pages/client', (req, res) => {
-    return res.render("html/client.html")
-})
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on("connection", (socket: Socket) => {
-    console.log("Se conectou", socket.id);
-})
-
-app.use(express.json());
-
-app.use(routes);
-
-http.listen(port, () => console.log(`Server running on port ${port}`));
+http.listen(3333, () => console.log(`Server running on port 3333`));
